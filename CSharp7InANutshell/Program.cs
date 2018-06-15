e2c7884f9128c1344b1b70ac5dd5ab7a4025f24d
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-06-15 19:39:27 +0200

    added up to page 25

--- CSharp7InANutshell/Program.cs
+++ CSharp7InANutshell/Program.cs
@@ -17,7 +17,7 @@ using System; // Importing namespace
 //    }
 //}
 
-// Page 15
+//// Page 15
 //public class Panda
 //{
 //    public string Name; // Instance field
@@ -30,15 +30,15 @@ using System; // Importing namespace
 //    }
 //}
 
-// Page 17
+//// Page 17
 //public struct Point { public int X, Y; }
 
-// Page 18
+//// Page 18
 //public class Point { public int X, Y; }
 
 class Test // Class declaration
 {
-    // Page 5
+    //// Page 5
 
     //static void Main() // Method declaration
     //{
@@ -53,7 +53,7 @@ class Test // Class declaration
         //Console.WriteLine(FeetToInches (30)); // 360
         //Console.WriteLine(FeetToInches (100)); // 1200
 
-        //page 12
+        ////page 12
 
         //string message = "Hello world";
         //string upperMessage = message.ToUpper();
@@ -63,8 +63,8 @@ class Test // Class declaration
         //message = message + x.ToString();
         //Console.WriteLine(message);     //Hello world2015
 
-        //bool simplevar = false;     //only takes either "true" or "false"
-        //if (simplevar)
+        //bool simplevar = false;     // "bool" Only takes either "true" or "false"
+        //if (simplevar)    // Because "simplevar" is false it will not print.
         //    Console.WriteLine("This will not print");
 
         //int x2 = 5000;
@@ -99,7 +99,7 @@ class Test // Class declaration
 
         //Console.WriteLine(Panda.Population);
 
-        // Page 17
+        //// Page 17
 
         //Point p1 = new Point();
         //p1.X = 7;
@@ -113,7 +113,7 @@ class Test // Class declaration
         //Console.WriteLine(p1.X);    // 9
         //Console.WriteLine(p2.X);    // 7
 
-        // Page 18
+        //// Page 18
 
         //Point p1 = new Point();
         //p1.X = 7;
@@ -132,9 +132,56 @@ class Test // Class declaration
 
         //Console.WriteLine(p.X); // NullReferenceException
 
+        //// Page 22
+        //Console.WriteLine(        1.0.GetType());     // Double (double)
+        //Console.WriteLine(       1E06.GetType());    // Double (double)
+        //Console.WriteLine(          1.GetType());   // Int32 (int)
+        //Console.WriteLine( 0xF0000000.GetType());  // UInt32 (unit)
+        //Console.WriteLine(0x100000000.GetType()); // Int64 (long)
+
+        //decimal d = 3.5M;   // M = Decimal (case-insensitive)
+        //Console.WriteLine(d);
+        //long i = 5; // Implicit conversion from int to long
+        //Console.WriteLine(i);
+
+        //float f = 4.5F;     // Won't compile without suffix
+        //decimal d = -1.23M; // Won't compile without suffix
+
+        //// Page 23
+        //int x = 12345;      // int is a 32-bit integral type
+        //long y = x;         // Implicit conversion to 64-bit int
+        //short z = (short)x; //Explicit conversion to 16-bit int
+
+        //int i1 = 100000001;
+        //Console.WriteLine(i1);
+        //float f = i1;       // Magnitude preserved precision lost
+        //Console.WriteLine(f);
+        //int i2 = (int)f;    // 100000000
+        //Console.WriteLine(i2);
+
+        //// Page 24
+        //int x = 0;
+        //Console.WriteLine(x++); // Outputs 0; x is now 1
+        //Console.WriteLine(++x); // Outputs 2; x is now 2
+        //Console.WriteLine(--x); // Outputs 1; x is now 1
+
+        //int a = int.MinValue; a--;
+        //Console.WriteLine(a == int.MaxValue);   //True
+
+        //// Page 25
+        //int a = 1000000, b = 1000000;
+
+        //int c = checked (a * b); // Checks just the expression
+
+        //checked     // Checks all expressions
+        //{           // In statement block.
+        //    c = a * b;
+        //}
+
+
     }
 
-    // Page 5 - 6
+    //// Page 5 - 6
     //static int FeetToInches (int feet)
     //{
     //    int inches = feet * 12;
